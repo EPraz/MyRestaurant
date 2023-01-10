b3b6c87be0ab95677c0c21d656545928fdd91a2e
Edwin Pile <epilepra@gmail.com>
2023-01-09 19:59:50 -0500

    update

--- MyRestaurant/settings.py
+++ MyRestaurant/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ LOGIN_REDIRECT_URL = "/"
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)j7328uv(yv74tsngp@4c$w&2ic*53d#l-i*egx83#ht4h0j_)'
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
